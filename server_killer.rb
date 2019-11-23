921cd1cb0b8ba0ab96b80bcade4ca0a49f1e530c
Fernando Aureliano <mail@fernandoaureliano.com>
2019-11-23 18:05:15 -0300

    creating the ruby script

---
+++ server_killer.rb
@@ -0,0 +1,6 @@
+port = ARGV.first || 8082
+system("sudo echo kill-server-on #{port}")
+
+pid = `sudo lsof -iTCP -sTCP:LISTEN -n -P | grep #{port} | awk '{ print $2 }' | head -n 1`.strip
+puts "PID: #{pid}"
+`kill -9 #{pid}` unless pid.empty?
