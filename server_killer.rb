5c4683efd5868ad117e2f8b396d847b85dd94092
Fernando Aureliano <mail@fernandoaureliano.com>
2019-11-23 18:26:33 -0300

    changing port

--- server_killer.rb
+++ server_killer.rb
@@ -1,4 +1,4 @@
-port = ARGV.first || 8082
+port = ARGV.first || 3000
 system("sudo echo kill-server-on #{port}")
 
 pid = `sudo lsof -iTCP -sTCP:LISTEN -n -P | grep #{port} | awk '{ print $2 }' | head -n 1`.strip
